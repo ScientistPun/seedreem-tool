cd3e4a9a8d278048fc42cf2f0cffb02ab5e4ddfd
koali <scientistpun@gamil.com>
2026-03-17 13:14:08 +0800

    v1.3.1

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow, ipcMain, dialog, globalShortcut } = require('electron')
 const path = require('path')
 const fs = require('fs')
+const yaml = require('js-yaml') // 已修复依赖
 
 let mainWindow
 
-function createWindow() {
+async function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1600,
     height: 1000,
@@ -15,16 +16,20 @@ function createWindow() {
     }
   })
 
-  mainWindow.loadFile('index.html')
+  await mainWindow.loadFile('index.html')
   mainWindow.setMenuBarVisibility(false)
-
-  // ✅ F12 打开开发者工具
-  globalShortcut.register('F12', () => {
-    mainWindow.webContents.toggleDevTools()
-  })
 }
 
-// 打开文件夹选择
+app.whenReady().then(() => {
+  createWindow()
+
+  globalShortcut.register('F12', () => {
+    if (mainWindow) {
+      mainWindow.webContents.toggleDevTools()
+    }
+  })
+})
+
 ipcMain.handle('open-directory', async () => {
   return await dialog.showOpenDialog({
     properties: ['openDirectory']
@@ -45,11 +50,17 @@ ipcMain.handle('getConfig', () => {
   const cfgPath = path.join(__dirname, 'config.yml')
   const defaultCfg = `
 api_key: ""
-auto_save: false
+dev_mode: true
+base_url: https://ark.cn-beijing.volces.com/api/v3/images/generations
+auto_save: true
+save_dir: 
 models:
-  v5: ""
-  v45: ""
-  v4: ""
+  v5: doubao-seedream-5-0-260128
+  v45: doubao-seedream-4-5-251128
+  v4: doubao-seedream-4-0-250828
+default:
+  output_format: png
+  watermark: false
 `
   if (!fs.existsSync(cfgPath)) fs.writeFileSync(cfgPath, defaultCfg, 'utf8')
   return fs.readFileSync(cfgPath, 'utf8')
@@ -62,15 +73,13 @@ ipcMain.handle('saveConfig', (event, content) => {
 
 ipcMain.handle('readReadme', () => {
   const readmePath = path.join(__dirname, 'README.md')
-  if (!fs.existsSync(readmePath)) return '# 使用说明\n\n暂无文档'
+  if (!fs.existsSync(readmePath)) return '# Seedream 图像生成工具'
   return fs.readFileSync(readmePath, 'utf8')
 })
 
 ipcMain.on('appExit', () => {
   app.quit()
 })
-
-app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -80,7 +89,6 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// 退出时注销快捷键
 app.on('will-quit', () => {
   globalShortcut.unregisterAll()
 })
